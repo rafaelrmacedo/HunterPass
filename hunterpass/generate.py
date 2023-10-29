aeb4b76e585de38c1c7b21c838bacc38889cd2d6
Rafael Macedo <rafaelrodriguesmacedo2004@gmail.com>
2023-10-28 23:11:39 -0300

    update: vars, animation remove & input try except

--- hunterpass/generate.py
+++ hunterpass/generate.py
@@ -4,73 +4,71 @@ import itertools
 import threading
 import sys
 
-def obter_chave_api():
+def get_api_key():
     return os.getenv('OPENAI_API_KEY')
 
-def loading_animation():
-    # Define os caracteres para a animação
-    animation_chars = itertools.cycle(['|', '/', '-', '\\'])
 
-    # Função que imprime a animação na tela
-    def animate():
-        for char in animation_chars:
-            sys.stdout.write('\r' + 'Gerando...' + char)
-            sys.stdout.flush()
-            threading.Event().wait(0.2)
+def generate_wordlist(output_file="dicionario.txt"):
+    api_key = get_api_key()
 
-    # Inicia a animação em uma thread
-    thread = threading.Thread(target=animate)
-    thread.start()
-    return thread
-
-def generate_wordlist(arquivo_saida="dicionario.txt"):
-    chave_api = obter_chave_api()
-
-    if chave_api is None:
+    if api_key is None:
         print("Erro: Chave API não encontrada. Por favor, defina a variável de ambiente OPENAI_API_KEY.")
         return
     
-    num_variacoes = int(input("Digite o número de variações: "))
-    comprimento_minimo = int(input("Digite o comprimento mínimo de cada palavra da senha: "))
-    comprimento_maximo = int(input("Digite o comprimento máximo de cada palavra da senha: "))
+    def get_input(mensagem):
+        while True:
+            try:
+                input_ = int(input(mensagem))
+                return input_
+            except ValueError:
+                print("Por favor, digite apenas números.")
 
-    openai.api_key = chave_api
 
-    mensagem_inicial = f"Gerar uma lista de palavras com as seguintes informações: " \
-    f"Tamanho da Lista (Variações de Senha) = {num_variacoes}" \
-    f"Comprimento Mínimo da Senha = {comprimento_minimo}." \
-    f"Comprimento Máximo da Senha = {comprimento_maximo}."\
-    "Misture as informações e não faça permutação.\n"
+    variation_num = get_input("Digite o número de variações: ")
+    min_length = get_input("Digite o comprimento mínimo de cada palavra da senha: ")
+    max_length = get_input("Digite o comprimento máximo de cada palavra da senha: ")
+    include_symbols = input("Quer utilizar simbolos especiais? (s/n): ").lower()
 
-    entrada_usuario = []
+    while include_symbols not in ('s', 'n'):
+        print("Por favor, responda com 's' para sim ou 'n' para não.")
+        include_symbols = input("Quer utilizar simbolos especiais? (s/n): ").lower()
+
+
+    openai.api_key = api_key
+
+    start_message = f"Generate a wordlist with the information below: " \
+    f"Wordlist Size (Variations) = {variation_num}" \
+    f"Minimun password length = {min_length}." \
+    f"Maximun password length = {max_length}." \
+    f"Symbols: {include_symbols}."
+
+    user_input = []
 
     while True:
-        informacao = input("Digite uma informação para a lista de palavras (Digite 'sair' para sair): ").lower()
-        if informacao.lower() == 'sair':
+        information = input("Digite uma informação para a lista de palavras (Digite 'sair' para sair): ").lower()
+        if information.lower() == 'sair':
             break
-        entrada_usuario.append(informacao)
+        user_input.append(information)
 
-    mensagem_inicial += "\n".join([f"Informação: {info}" for info in entrada_usuario])
+    start_message += "\n".join([f"Information: {info}" for info in user_input])
+    start_message += "\n Mix this informations and don't make simple permutations." \
 
-    mensagens = [{"role": "user", "content": mensagem_inicial}]
+    mensagens = [{"role": "user", "content": start_message}]
 
-    # Inicia a animação
-    animation_thread = loading_animation()
+    print("\nGerando a lista de palavras...")
 
-    resposta = openai.ChatCompletion.create(
+    response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=mensagens,
     )
     
-    texto_gerado = resposta.choices[0].message['content'].strip()
+    generated_text = response.choices[0].message['content'].strip()
 
-    # Aguarda a animação terminar
-    animation_thread.join()
+    with open(output_file, 'w') as f:
+        f.write(generated_text)
 
-    with open(arquivo_saida, 'w') as f:
-        f.write(texto_gerado)
+    print("\nGeração completada.")
 
-    print("\nGeração completada")
 
 if __name__ == "__main__":
     generate_wordlist()
